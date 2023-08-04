8814659a11f43b466896da281ee99a34a8cc700c
Shepherd Xie <shepherd.xie@gmail.com>
2023-08-04 14:11:37 +0800

    init

---
+++ src/main/java/com/orkva/xmall/inventory/XmallInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.orkva.xmall.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmallInventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XmallInventoryApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/orkva/xmall/inventory/entity/pojo/BatchesInventory.java
@@ -0,0 +1,32 @@
+package com.orkva.xmall.inventory.entity.pojo;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.Instant;
+
+/**
+ * BatchesInventory
+ *
+ * @author Shepherd Xie
+ * @version 2023/8/4
+ */
+@Entity
+@Data
+@Table(name = "tb_batches_inventories")
+public class BatchesInventory {
+    @Column(unique = true)
+    private String lotNumber;
+    private Integer skuId;
+    private Integer total;
+    private Integer remaining;
+    private Integer outbound;
+    private Integer available;
+    private Integer locked;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Instant createdDate;
+    private Instant updatedDate;
+}
