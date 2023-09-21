f5e6f72fd5f6f87202c0b8f3de092acdd6ff0559
Shepherd Xie <shepherd.xie@gmail.com>
2023-09-21 16:53:12 +0800

    feat: update

--- api/src/main/java/com/orkva/projects/xmall/inventory/api/controller/InventoryController.java
+++ api/src/main/java/com/orkva/projects/xmall/inventory/api/controller/InventoryController.java
@@ -19,13 +19,11 @@ import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Principal;
 import java.util.List;
 
 /**
@@ -47,18 +45,24 @@ public class InventoryController implements InventoryContractController {
     @Autowired
     private BatchesInventoriesService batchesInventoriesService;
 
-    @PreAuthorize("authentication")
-    @GetMapping("/user")
-    public JsonResult<Principal> user(Principal principal) {
-        return JsonResult.ok(principal);
-    }
-
+    /**
+     * 获取批次库存列表
+     *
+     * @return JsonResult 包含批次库存记录的列表
+     */
     @GetMapping(Routers.Inventory.LIST_BATCHES_INVENTORIES)
     @Override
     public JsonResult<List<BatchesInventoryRecord>> listBatchesInventories() {
         return JsonResult.ok(InventoryConvertor.toBatchesInventoryRecords(batchesInventoryRepository.findAll()));
     }
 
+    /**
+     * 分页获取批次库存列表
+     *
+     * @param batchesInventoriesQuery 查询条件
+     * @param pageable                分页参数
+     * @return JsonResult 包含分页批次库存记录的结果
+     */
     @GetMapping(Routers.Inventory.PAGE_BATCHES_INVENTORIES)
     public JsonResult<Page<BatchesInventoryRecord>> pageBatchesInventories(
             BatchesInventoriesQuery batchesInventoriesQuery,
@@ -69,12 +73,24 @@ public class InventoryController implements InventoryContractController {
         return JsonResult.ok(InventoryConvertor.toBatchesInventoryRecords(batchesInventoryPage, pageable));
     }
 
+    /**
+     * 获取可用的批次库存列表（根据SKU ID）
+     *
+     * @param skuId SKU ID
+     * @return JsonResult 包含可用批次库存记录的列表
+     */
     @GetMapping(Routers.Inventory.LIST_AVAILABLE_BATCHES_INVENTORIES)
     @Override
     public JsonResult<List<BatchesInventoryRecord>> listAvailableBatchesInventoriesBySkuId(@NotNull Long skuId) {
         return JsonResult.ok(InventoryConvertor.toBatchesInventoryRecords(batchesInventoryRepository.findAvailableBySkuId(skuId)));
     }
 
+    /**
+     * 获取批次库存变更日志列表
+     *
+     * @param batchesInventoryChangeLogRecord 查询条件
+     * @return JsonResult 包含批次库存变更日志记录的列表
+     */
     @GetMapping(Routers.Inventory.LIST_BATCHES_INVENTORY_CHANGE_LOGS)
     @Override
     public JsonResult<List<BatchesInventoryChangeLogRecord>> listBatchesInventoryChangeLogs(
@@ -85,12 +101,23 @@ public class InventoryController implements InventoryContractController {
         return JsonResult.ok(InventoryConvertor.toBatchesInventoryChangeLogRecords(batchesInventoryChangeLogs));
     }
 
+    /**
+     * 获取SKU库存列表
+     *
+     * @return JsonResult 包含SKU库存记录的列表
+     */
     @GetMapping(Routers.Inventory.LIST_SKU_INVENTORIES)
     @Override
     public JsonResult<List<SkuInventoryRecord>> listSkuInventories() {
         return JsonResult.ok(InventoryConvertor.toSkuInventoryRecords(skuInventoryRepository.findAll()));
     }
 
+    /**
+     * 购买库存
+     *
+     * @param paramsRecord 购买参数记录
+     * @return JsonResult 包含购买后的批次库存记录
+     */
     @PostMapping(Routers.Inventory.PURCHASE)
     @Override
     public JsonResult<BatchesInventoryRecord> purchase(@RequestBody InventoryPurchaseParamsRecord paramsRecord) {
@@ -98,6 +125,12 @@ public class InventoryController implements InventoryContractController {
         return JsonResult.ok(InventoryConvertor.toBatchesInventoryRecord(batchesInventory));
     }
 
+    /**
+     * 锁定库存
+     *
+     * @param paramsRecord 锁定参数记录
+     * @return JsonResult 包含锁定后的批次库存记录
+     */
     @PostMapping(Routers.Inventory.LOCKED)
     @Override
     public JsonResult<BatchesInventoryRecord> locked(@RequestBody InventoryChangeParamsRecord paramsRecord) {
@@ -105,6 +138,12 @@ public class InventoryController implements InventoryContractController {
         return JsonResult.ok(InventoryConvertor.toBatchesInventoryRecord(batchesInventory));
     }
 
+    /**
+     * 取货库存
+     *
+     * @param paramsRecord 取货参数记录
+     * @return JsonResult 包含取货后的批次库存记录
+     */
     @PostMapping(Routers.Inventory.PICKUP)
     @Override
     public JsonResult<BatchesInventoryRecord> pickup(@RequestBody InventoryChangeParamsRecord paramsRecord) {
@@ -112,6 +151,12 @@ public class InventoryController implements InventoryContractController {
         return JsonResult.ok(InventoryConvertor.toBatchesInventoryRecord(batchesInventory));
     }
 
+    /**
+     * 退货库存
+     *
+     * @param paramsRecord 退货参数记录
+     * @return JsonResult 包含退货后的批次库存记录
+     */
     @PostMapping(Routers.Inventory.RECEDE)
     @Override
     public JsonResult<BatchesInventoryRecord> recede(@RequestBody InventoryChangeParamsRecord paramsRecord) {
